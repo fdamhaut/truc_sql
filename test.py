b1587096d24178fed0e745c1601d15a9edfdfcdb
Florian Damhaut <flda@odoo.com>
2023-04-19 13:13:18 +0200

    next

--- test.py
+++ test.py
@@ -16,29 +16,28 @@ def show_row(log):
         (log['id'], log['create_date'], log['event_date'], log['event_type'], log['amount_signed'] or 0, log['recurring_monthly']))
 
 def show_table(logs, error='All right'):
-    global last_code
-    if (last_code is None) or (last_code != logs[0]['ooid']):
-        last_code = logs[0]['ooid']
-        print() # new line at each contract
     print('Order %-8s %-16s %s %s:'% (logs[0]['order_id'], logs[0]['ooid'], logs[0]['currency_id'], error))
     for log in logs:
         show_row(log)
+
+def show_all():
+    for order, logs in orders.items():
+        show_table(logs)
+    print()
 
 
 orders = defaultdict(list)
 order_per_origin = defaultdict(list)
 has_next = set()
 
-# ('M21043025842286','M22011035087550', 'M22070141784236', 'M21101831769180', 'M21043025842286', 'M1701132758517')
-codes = ('M21043025842286','M22011035087550', 'M22070141784236', 'M21101831769180', 'M21043025842286', 'M1701132758517')
-
 # After log_1.sql
 
 
 cr.execute('''
-    select *, coalesce(origin_order_id, order_id) as ooid
-    from sale_order_log
-    order by coalesce(origin_order_id, order_id), order_id, create_date, id''')
+    SELECT *, coalesce(origin_order_id, order_id) as ooid
+    FROM sale_order_log
+    ORDER BY coalesce(origin_order_id, order_id), order_id, create_date, id
+    ''')
 
 logs = cr.fetchall()
 
@@ -55,13 +54,12 @@ for log in logs:
     orders[order_id].append(log)
 
 
-print(prec_order[2423043], prec_order[2296619])
-
+print(order_per_origin[2277795])
 
 # Fix Transfer
 for order_id,logs in orders.items():
-
     # If there is a transfer and expansion in same transaction: we might need to merge them
+    # Maybe useless now
     for i in range(1, len(logs)-1):
         if logs[i]['create_date'] == logs[i+1]['create_date'] and \
             logs[i]['event_type'] == '3_transfer' and \
@@ -81,11 +79,12 @@ for order_id,logs in orders.items():
                 set id = %s
                 where id = %s
                 ''', (logs[i+1]['id'], -1))
-            
+
             logs[i]['id'], logs[i+1]['id'] = logs[i+1]['id'], logs[i]['id']
             logs[i], logs[i+1] = logs[i+1], logs[i]
 
     # Try to match transfers of new orders
+
     before = prec_order.get(logs[0]['order_id'], None)
     if before and orders[before]:
         # last line before is not a transfer
@@ -150,6 +149,7 @@ for order_id,logs in orders.items():
                     'new_enterprise_user': 0,}] + logs
             orders[order_id] = logs
 
+
         # We ensure that the 'ending' event of the parent correspond to the 'begin' of this one 
         # Add condition to avoid useless execute
         logs0[-1]['create_date'] = logs[0]['create_date']
@@ -174,47 +174,37 @@ for order_id,logs in orders.items():
                 logs0[-1]['id'])
             )
 
+        if len(logs0) > 2 and\
+            logs0[-2]['event_type'] in ('1_expansion', '15_contraction') and\
+            logs0[-2]['create_date'] == logs0[-1]['create_date'] and\
+            logs0[-2]['create_date'] == logs0[0]['create_date'] and\
+            logs0[-2]['id'] > logs0[-1]['id']:
+
+            cr.execute('''update sale_order_log
+                        set id = %s
+                        where id = %s
+                        ''', (-1, logs0[-1]['id']))
+            cr.execute('''update sale_order_log
+                        set id = %s
+                        where id = %s
+                        ''', (logs0[-1]['id'], logs0[-2]['id']))
+            cr.execute('''update sale_order_log
+                        set id = %s
+                        where id = %s
+                        ''', (logs0[-2]['id'], -1))
+
+            logs0[-1]['id'], logs0[-2]['id'] = logs0[-2]['id'], logs0[-1]['id']
+
+
         # Reconcile Transfer
-        if len(logs) > 1 and logs[0]['event_type'] == '3_transfer' and\
+        if len(logs) and logs[0]['event_type'] == '3_transfer' and\
             logs[0]['currency_id'] == orders[before][-1]['currency_id']:
 
             old_mrr = -orders[before][-1]['amount_signed']
-            diff = logs[0]['recurring_monthly'] - old_mrr
-            logs[0]['amount_signed'] = logs[0]['recurring_monthly'] = old_mrr
-            logs[1]['amount_signed'] = logs[1]['recurring_monthly'] - old_mrr
-            logs[1]['event_type'] = '1_expansion' if logs[1]['amount_signed'] >= 0 else '15_contraction'
-            orders[before][-1]['event_date'] = logs[0]['event_date']
-
-            cr.execute('''
-                UPDATE sale_order_log 
-                    set amount_signed = %s, 
-                        recurring_monthly = %s
-                    where id = %s''', 
-                (old_mrr, old_mrr, logs[0]['id']))
-            cr.execute('''
-                UPDATE sale_order_log 
-                    set amount_signed = %s,
-                        event_type = %s
-                    where id = %s''', 
-                (logs[1]['amount_signed'], logs[1]['event_type'], logs[1]['id']))
-            cr.execute('''
-                UPDATE sale_order_log 
-                    set event_date = %s
-                    where id = %s''', 
-                (logs[0]['event_date'], orders[before][-1]['id']))
-
-        elif len(logs) and logs[0]['event_type'] == '3_transfer' and\
-            logs[0]['currency_id'] == orders[before][-1]['currency_id']:
-
-            old_mrr = - orders[before][-1]['amount_signed']
             new_mrr = logs[0]['recurring_monthly']
 
-            cr.execute('''UPDATE sale_order_log 
-                    set amount_signed = %s, 
-                        recurring_monthly = %s
-                    where id = %s''', 
-                (old_mrr, old_mrr, logs[0]['id']))
             orders[before][-1]['event_date'] = logs[0]['event_date']
+
             cr.execute('''
                 UPDATE sale_order_log 
                     set event_date = %s
@@ -222,14 +212,22 @@ for order_id,logs in orders.items():
                 (logs[0]['event_date'], orders[before][-1]['id']))
 
             if new_mrr != old_mrr:
-                # TODO insert MRR Change
                 l = logs[0]
+                cr.execute('''UPDATE sale_order_log 
+                    set amount_signed = %s, 
+                        recurring_monthly = %s
+                    where id = %s''', 
+                (old_mrr, old_mrr, l['id']))
+
+                l['amount_signed'] = l['recurring_monthly'] = old_mrr
+                # TODO insert MRR Change
                 cr.execute('''
                 INSERT INTO sale_order_log(
                     order_id,
                     origin_order_id,
                     subscription_code,
                     event_date,
+                    create_date,
                     currency_id,
                     subscription_state,
                     recurring_monthly,
@@ -238,9 +236,44 @@ for order_id,logs in orders.items():
                     amount_contraction,
                     event_type
                 )
-                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
-                (l['order_id'], l['origin_order_id'], l['subscription_code'], l['event_date'],
-                 l['currency_id'], '5_renewed', new_mrr, new_mrr - old_mrr, 0, new_mrr - old_mrr, '1_expansion'))
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                RETURNING id''',
+                (l['order_id'], l['origin_order_id'], l['subscription_code'], l['event_date'], l['create_date'],
+                 l['currency_id'], '3_progress', new_mrr, new_mrr - old_mrr, 0, new_mrr - old_mrr, '1_expansion'))
+                ids = cr.fetchall()[0]['id']
+                orders[order_id] = [logs[0]] + [{ 'id': ids,
+                    'create_date': l['create_date'],
+                    'order_id': l['order_id'],
+                    'ooid': l['ooid'],
+                    'subscription_code': l['subscription_code'],
+                    'event_date': l['event_date'],
+                    'currency_id': l['currency_id'],
+                    'subscription_state': '3_progress',
+                    'recurring_monthly': new_mrr,
+                    'amount_signed': new_mrr-old_mrr,
+                    'event_type': '1_expansion',
+                    'new_enterprise_user': 0,}] + logs[1:]
+                logs = orders[order_id]
+
+                if len(logs) > 2 and\
+                    logs[1]['create_date'] == logs[2]['create_date'] and\
+                    logs[2]['event_type'] in ('3_transfer', '2_churn'):
+
+                    cr.execute('''update sale_order_log
+                        set id = %s
+                        where id = %s
+                        ''', (-1, logs[1]['id']))
+                    cr.execute('''update sale_order_log
+                        set id = %s
+                        where id = %s
+                        ''', (logs[1]['id'], logs[2]['id']))
+                    cr.execute('''update sale_order_log
+                        set id = %s
+                        where id = %s
+                        ''', (logs[2]['id'], -1))
+            
+                    logs[1]['id'], logs[2]['id'] = logs[2]['id'], logs[1]['id']
+
 
 print('Between Done')
 
@@ -280,19 +313,22 @@ for order_id,logs in orders.items():
 print('IN Done')
 
 
+
 orders = defaultdict(list)
 order_per_origin = defaultdict(list)
 has_next = set()
+prec_order = {}
+
 
 cr.execute('''
-    select *, coalesce(origin_order_id, order_id) as ooid
-    from sale_order_log 
-    order by coalesce(origin_order_id, order_id), order_id, create_date, id''')
+    SELECT *, coalesce(origin_order_id, order_id) as ooid
+    FROM sale_order_log
+    ORDER BY coalesce(origin_order_id, order_id), order_id, create_date, id
+    ''')
 
 logs = cr.fetchall()
 
 
-prec_order = {}
 for log in logs:
     order_id = log['order_id']
     origin = log['ooid']
@@ -302,6 +338,9 @@ for log in logs:
             has_next.add(order_per_origin[origin][-1])
         order_per_origin[origin].append(order_id)
     orders[order_id].append(log)
+
+for order_id in order_per_origin[2277795]:
+    show_table(orders[order_id])
 
 
 for order_id,logs in orders.items():
@@ -315,4 +354,4 @@ for order_id,logs in orders.items():
         assert 0 == 1
 
 
-conn.commit()
+conn.rollback()
